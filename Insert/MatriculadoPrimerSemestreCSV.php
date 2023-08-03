fb3feae70ac9488a77f06074c25659fc1364f08f
Angiee Carrión <angie13071@gmail.com>
2023-08-02 21:26:28 -0500

    se corrige error de datos vacios en primiparos

--- Insert/MatriculadoPrimerSemestreCSV.php
+++ Insert/MatriculadoPrimerSemestreCSV.php
@@ -25,7 +25,7 @@ for ($i = 2; $i < count($file_data); $i++) {
     $row = array_map(function ($item) {
         return str_replace('"', '', $item);
     }, $row);
-
+ 
     $id_primiparo = $row[3];
     $id_estudiante = $row[4];
     $periodo = $row[4];; // Asigna el ID del periodo correspondiente
@@ -50,34 +50,36 @@ for ($i = 2; $i < count($file_data); $i++) {
     $stmt_check_existing->bind_result($existing_count);
     $stmt_check_existing->fetch();
     $stmt_check_existing->close();
-
-    if ($existing_count > 0) {
-        // El estudiante ya existe en la tabla 'primiparo', mostrar una alerta o hacer otra acción si lo deseas
-        echo "<span style='font-size: 24px; color: orange;'>¡ALERTA!</span> El estudiante matriculado actualmente con ID $id_estudiante ya existe en la tabla PRIMIPARO. Se omitirá la inserción.<br>"; 
-        $insertion_error = true;
-    } else {
-        // Preparar la consulta SQL para insertar el estudiante en la tabla 'primiparo'
-        $sql_insert = "INSERT INTO primiparo (id_primiparo, id_estudiante, id_periodo) VALUES (?, ?, ?)";
-
-        // Preparar la sentencia
-        $stmt_insert = $conn->prepare($sql_insert);
-
-        // Asignar los valores a los parámetros
-        $stmt_insert->bind_param("iii", $id_primiparo, $id_estudiante, $id_periodo);
-
-        // Ejecutar la consulta
-        if (!$stmt_insert->execute()) {
-            // Hubo un error durante la inserción
-            //TODO:Validar los no insertados
-           $insertion_error =true;
-            echo "<span style='font-size: 24px; color: red;'>X ERROR</span> El estudiante nuevo con ID $id_primiparo, $id_estudiante, $id_periodo no se pudo insertar en la tabla PRIMIPARO: ". $stmt_insert->error ,"<br>";
-        }else{
-            $insertion_error =false;
+    if (!empty($id_primiparo) && !empty($id_estudiante) && !empty($periodo)) {
+        if ($existing_count > 0) {
+            // El estudiante ya existe en la tabla 'primiparo', mostrar una alerta o hacer otra acción si lo deseas
+            echo "<span style='font-size: 24px; color: orange;'>¡ALERTA!</span> El estudiante matriculado actualmente con ID $id_estudiante ya existe en la tabla PRIMIPARO. Se omitirá la inserción.<br>"; 
+            $insertion_error = true;
+        } else {
+            // Preparar la consulta SQL para insertar el estudiante en la tabla 'primiparo'
+            $sql_insert = "INSERT INTO primiparo (id_primiparo, id_estudiante, id_periodo) VALUES (?, ?, ?)";
+    
+            // Preparar la sentencia
+            $stmt_insert = $conn->prepare($sql_insert);
+    
+            // Asignar los valores a los parámetros
+            $stmt_insert->bind_param("iii", $id_primiparo, $id_estudiante, $id_periodo);
+    
+            // Ejecutar la consulta
+            if (!$stmt_insert->execute()) {
+                // Hubo un error durante la inserción
+                //TODO:Validar los no insertados
+               $insertion_error =true;
+                echo "<span style='font-size: 24px; color: red;'>X ERROR</span> El estudiante nuevo con ID $id_primiparo, $id_estudiante, $id_periodo no se pudo insertar en la tabla PRIMIPARO: ". $stmt_insert->error ,"<br>";
+            }else{
+                $insertion_error =false;
+            }
+    
+            // Cerrar la sentencia
+            $stmt_insert->close();
         }
-
-        // Cerrar la sentencia
-        $stmt_insert->close();
     }
+   
 }
 
 // Cerrar la conexión a la base de datos después de haber procesado todos los datos
