9221851d597bd96df770bea28478652b4c729665
Angiee Carrión <angie13071@gmail.com>
2023-08-07 20:59:06 -0500

    Se actualiza filtro por carrera de permanencia por cohorte

--- View_Permanencia_Cohorte.php
+++ View_Permanencia_Cohorte.php
@@ -26,6 +26,13 @@
                 <option value="bar">Gráfico de columnas</option>
             </select>
         </div>
+        <div>
+            <select id="chart-carrera">
+                <option value="all">Todas las carreras</option>
+                <option value="tec">Tecnología en sistematización de datos</option>
+                <option value="ing">Ingeniería telemática</option>
+            </select>
+        </div>
         <button class="arrow-button" onclick="goBack()">&#8592;</button>
     </div>
 
@@ -102,20 +109,22 @@
 
                ?>
 
+
     // Crear la gráfica inicial
     var ctx = document.getElementById('permanencia-chart').getContext('2d');
     var chartTypeSelect = document.getElementById('chart-type');
+    var chartCarreraSelect = document.getElementById('chart-carrera');
     var chart;
 
     // Función para crear el gráfico
-    function createChart(chartType, labels, datasets) {
+    function createChart(chartType, labels, dataset) {
         if (chart) {
             chart.destroy(); // Destruir el gráfico existente si ya se ha creado
         }
 
         var data = {
             labels: labels,
-            datasets: datasets
+            datasets: [dataset]
         };
 
         var options = {
@@ -129,43 +138,40 @@
             options: options
         });
     }
-    createChart(chartTypeSelect.value, <?php echo json_encode($cohortes); ?>, [{
-            label: 'Todas las carreras',
-            data: <?php echo json_encode($permanencias); ?>,
-            backgroundColor: 'rgba(255, 159, 49, 0.87)',
-            borderColor: 'rgba(255, 159, 49, 0.87)',
-            borderWidth: 1
-        },
-        {
-            label: 'Tecnología en sistematización de datos',
-            data: <?php echo json_encode($permanenciasTec); ?>,
-            backgroundColor: 'rgba(54, 162, 235, 0.5)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1
-        },
-        {
-            label: 'Ingeniería telemática',
-            data: <?php echo json_encode($permanenciasIng); ?>,
-            backgroundColor: 'rgba(0, 255, 0, 0.58)',
-            borderColor: 'rgba(0, 255, 0, 0.58)',
-            borderWidth: 1
-        }
-
-    ]);
-    // Evento de cambio de tipo de gráfico
-    chartTypeSelect.addEventListener('change', function() {
+    // Función para actualizar el gráfico con la carrera seleccionada
+    function updateChart() {
         var selectedType = chartTypeSelect.value;
-        createChart(selectedType, <?php echo json_encode($cohortes); ?>,
-            <?php echo json_encode($permanencias); ?>);
-    });
+        var selectedCarrera = chartCarreraSelect.value;
+
+        var dataset = {
+            label: selectedCarrera === 'all' ? 'TODAS LAS CARRERAS' : selectedCarrera === 'tec' ?
+                'TECNOLOGIA EN SISTEMATIZACION DE DATOS' : 'INGENIERIA EN TELEMATICA',
+            data: selectedCarrera === 'all' ? <?php echo json_encode($permanencias); ?> : selectedCarrera ===
+                'tec' ? <?php echo json_encode($permanenciasTec); ?> : <?php echo json_encode($permanenciasIng); ?>,
+            backgroundColor: selectedCarrera === 'all' ? 'rgba(255, 159, 49, 0.87)' : selectedCarrera === 'tec' ?
+                'rgba(54, 162, 235, 0.5)' : 'rgba(0, 255, 0, 0.58)',
+            borderColor: selectedCarrera === 'all' ? 'rgba(255, 159, 49, 0.87)' : selectedCarrera === 'tec' ?
+                'rgba(54, 162, 235, 1)' : 'rgba(0, 255, 0, 0.58)',
+            borderWidth: 1
+        };
+
+        createChart(selectedType, <?php echo json_encode($cohortes); ?>, dataset);
+    }
+
+    // Evento de cambio de tipo de gráfico
+    chartTypeSelect.addEventListener('change', updateChart);
 
     // Evento de cambio de carrera
-    // chartSelectCarrer.addEventListener('change', function() {
-    //     updateChart();
-    // });
+    chartCarreraSelect.addEventListener('change', updateChart);
 
     // Crear el gráfico inicial
-    // createChart(chartTypeSelect.value, <?php echo json_encode($cohortes); ?>,<?php echo json_encode($permanencias); ?>);
+    createChart(chartTypeSelect.value, <?php echo json_encode($cohortes); ?>, {
+        label: 'Todas las carreras',
+        data: <?php echo json_encode($permanencias); ?>,
+        backgroundColor: 'rgba(255, 159, 49, 0.87)',
+        borderColor: 'rgba(255, 159, 49, 0.87)',
+        borderWidth: 1
+    });
     </script>
     <script>
     function goBack() {
