aff2326e69426505abc65deb86be5f6a7520f590
Angiee Carrión <angie13071@gmail.com>
2023-08-13 23:09:24 -0500

    se actualizan insert de graduados a graduado

--- Insert/Insert_EgresadoCSV_Graduado.php
+++ Insert/Insert_EgresadoCSV_Graduado.php
@@ -109,7 +109,7 @@ if($insertion_alert){
     echo '<div style="background-color: #FBFFBA; color: black; padding: 10px; text-align: center;border-radius: 0.8rem;
         border: 2px solid orange; width: 70rem; position: relative;margin-bottom: 2rem;">
         <span style="font-size: 2rem;color:orange">¡ALERTA!</span><br>
-         Los graduados con el ID: '.$insertion_alert .'ya existen en la tabla GRADUADO. Se omitirá la inserción.
+         Los graduados con el ID: '.$alerts_by_student .'ya existen en la tabla GRADUADO. Se omitirá la inserción.
         <div style="position: absolute; top: 1rem; left: 1rem; font-size: 3rem;color:orange">❼</div>
         <div style="position: absolute;  left: 50%;">
          <span style="font-size: 4rem;">&#8595;</span>
@@ -119,7 +119,7 @@ if($insertion_alert){
     echo '<div style="background-color: #FFE1E1; color: black; padding: 10px; text-align: center;border-radius: 0.8rem;
     border: 2px solid rgba(255, 99, 132, 1); width: 70rem; position: relative;margin-bottom: 2rem;">
     <span style="font-size: 2rem;color:rgba(255, 99, 132, 1)">X ERROR</span><br>
-    Los graduados con ID: .'.$insertion_error.' no se pueden insertar en la tabla GRADUADO: ". '.$stmt->error.',"<br>";
+    Los graduados con ID: .'.$errors_by_student.' no se pueden insertar en la tabla GRADUADO: ". '.$stmt->error.',"<br>";
     <div style="position: absolute; top: 1rem; left: 1rem; font-size: 3rem;color:rgba(255, 99, 132, 1)">❼</div>
     <div style="position: absolute;  left: 50%;">
      <span style="font-size: 4rem;">&#8595;</span>
