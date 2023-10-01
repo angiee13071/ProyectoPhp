4bb96ec391ad2066a650acb125258099c4246b05
Angiee Carrión <angie13071@gmail.com>
2023-10-01 18:42:51 -0500

    Se actualizan calculos de propedio ponderado deserción

--- View_Desercion_Promedio.php
+++ View_Desercion_Promedio.php
@@ -5,20 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Promedio acumulado de deserción estudiantil</title>
+    <title>Promedio desercion</title>
     <link rel="stylesheet" href="styles.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
 </head>
+<?php include 'header.html'; ?>
 
 <body>
-    <?php include 'header.html'; ?>
     <div class="card">
         <div class="title">
-            <h1>Promedio acumulado de deserción estudiantil</h1>
+            <h1>Promedio ponderado de deserción</h1>
         </div>
         <div class="chart-container">
-            <canvas id="retencion-chart"></canvas>
+            <canvas id="desercion-chart"></canvas>
         </div>
         <div>
             <select id="chart-type">
@@ -26,7 +25,6 @@
                 <option value="bar">Gráfico de columnas</option>
             </select>
         </div>
-
         <div>
             <select id="chart-carrera">
                 <option value="all">Todas las carreras</option>
@@ -38,230 +36,206 @@
     </div>
 
     <script>
-    // Obtener los datos de la tabla 'total'
+    // Obtener los datos de la tabla 'graduado' y 'estudiante'
     <?php
-    include "ConexionBD.php"; // Incluye el archivo de conexión a la base de datos
+        include "ConexionBD.php"; // Incluye el archivo de conexión a la base de datos
 // Crear una instancia de la clase DatabaseConnection
 $dbConnection = new DatabaseConnection();
 $conn = $dbConnection->getDBConnection();
-    $periodo = [];
-    $desercion = [];
-    
-    $periodoTec = [];
-    $desercionTec = [];
-    
-    $periodoIng = [];
-    $desercionIng = [];
-    
-    function fetchData($carrera) {
-     $query="SELECT ";
-        global $conn, $periodo,$desercion,$periodoTec,$desercionTec,$periodoIng,$desercionIng;
-        if ($carrera === 'all') {
-            $query .= "  CONCAT(t1.anio, '-', t1.semestre) AS periodos,
-            CASE
-                WHEN t2.acum_matriculados > 0 THEN
-                    ROUND((t2.acum_retirados / t2.acum_matriculados) * 100, 2)
-                ELSE
+        //grafica 1
+        $cohortes = [];
+        $desercions = [];
+        //grafica 2
+        $cohortesTec = [];
+        $desercionsTec = [];
+        //grafica 3
+        $cohortesIng = [];
+        $desercionsIng = [];
+ 
+
+        // Función para obtener los datos dependiendo de la carrera seleccionada
+        function fetchData($carrera) {
+            global $conn, $cohortes, $desercions,$cohortesTec,$desercionsTec,$cohortesIng,$desercionsIng;
+
+            $query = "SELECT
+         ";
+
+            if ($carrera === 'all') {
+              $query .="     CONCAT(periodo.anio, '-', periodo.semestre) AS periodos,
+              NULL AS id_programa,
+              SUM(t.retirados) AS desertores_total,
+              SUM(t.matriculados) AS matriculados_total,
+              CAST(
+                COALESCE(
+                  CASE
+                    WHEN SUM(t.retirados) = 0 OR SUM(t.matriculados) = 0 THEN 0
+                    ELSE (SUM(t.retirados) / NULLIF(SUM(t_anterior.matriculados), 0)) * 100
+                  END,
+                  0
+                ) AS UNSIGNED
+              ) AS tasa_desercion
+            FROM total t
+            JOIN periodo ON t.id_periodo = periodo.id_periodo
+            LEFT JOIN (
+              SELECT id_programa, matriculados, id_periodo
+              FROM total
+            ) AS t_anterior ON t.id_programa = t_anterior.id_programa AND t.id_periodo = t_anterior.id_periodo + 1
+            GROUP BY periodo.anio, periodo.cohorte
+            ORDER BY periodos
+            LIMIT 0, 1000;";
+            }elseif($carrera === 'tec'){
+                $query .= " CONCAT(periodo.anio, '-', periodo.semestre) AS periodos,
+                NULL AS id_programa,
+                SUM(t.retirados) AS desertores_total,
+                SUM(t.matriculados) AS matriculados_total,
+                CAST(
+                  COALESCE(
+                    CASE
+                      WHEN SUM(t.retirados) = 0 OR SUM(t.matriculados) = 0 THEN 0
+                      ELSE (SUM(t.retirados) / NULLIF(SUM(t_anterior.matriculados), 0)) * 100
+                    END,
                     0
-            END AS tasa_desercion
-        FROM
-            periodo t1
-        LEFT JOIN (
-            SELECT
-                m.id_periodo,
-                COUNT(DISTINCT m.id_estudiante) AS acum_matriculados,
-                COUNT(DISTINCT r.id_retiro) AS acum_retirados
-            FROM
-                matriculado m
-            LEFT JOIN retirado r ON m.id_periodo = r.id_periodo
-            GROUP BY
-                m.id_periodo
-        ) t2 ON t1.id_periodo = t2.id_periodo
-        ORDER BY
-            t1.anio, t1.semestre
-        LIMIT 0, 1000;
-           
-        
-";
-        }elseif($carrera === 'tec'){
-            $query .=  " CONCAT(t1.anio, '-', t1.semestre) AS periodos,
-            pr.nombre AS carrera,
-            CASE
-                WHEN t2.acum_matriculados > 0 THEN
-                    ROUND((t2.acum_retirados / t2.acum_matriculados) * 100, 2)
-                ELSE
+                  ) AS UNSIGNED
+                ) AS tasa_desercion
+              FROM total t
+              JOIN periodo ON t.id_periodo = periodo.id_periodo
+              LEFT JOIN (
+                SELECT id_programa, matriculados, id_periodo
+                FROM total
+              ) AS t_anterior ON t.id_programa = t_anterior.id_programa AND t.id_periodo = t_anterior.id_periodo + 1
+              WHERE t.id_programa = 578 -- Filtrar por id_programa igual a 578
+              GROUP BY periodo.anio, periodo.cohorte
+              ORDER BY periodos
+              LIMIT 0, 1000;
+              ";
+            }elseif($carrera === 'ing'){
+                $query .= " CONCAT(periodo.anio, '-', periodo.semestre) AS periodos,
+                NULL AS id_programa,
+                SUM(t.retirados) AS desertores_total,
+                SUM(t.matriculados) AS matriculados_total,
+                CAST(
+                  COALESCE(
+                    CASE
+                      WHEN SUM(t.retirados) = 0 OR SUM(t.matriculados) = 0 THEN 0
+                      ELSE (SUM(t.retirados) / NULLIF(SUM(t_anterior.matriculados), 0)) * 100
+                    END,
                     0
-            END AS tasa_desercion
-        FROM
-            periodo t1
-        LEFT JOIN (
-            SELECT
-                p.id_periodo,
-                SUM(DISTINCT pr.id_matricula) AS acum_matriculados,
-                SUM(DISTINCT r.id_retiro) AS acum_retirados
-            FROM
-                periodo p
-            LEFT JOIN matriculado pr ON p.id_periodo = pr.id_periodo
-            LEFT JOIN retirado r ON p.id_periodo = r.id_periodo
-            GROUP BY
-                p.id_periodo
-        ) t2 ON t1.id_periodo = t2.id_periodo
-        LEFT JOIN total t ON t1.id_periodo = t.id_periodo
-        LEFT JOIN programa pr ON t.id_programa = pr.id_programa
-        WHERE
-            pr.nombre = 'TECNOLOGIA EN SISTEMATIZACION DE DATOS (CICLOS PROPEDEUTICOS)' 
-        ORDER BY
-            t1.anio, t1.semestre
-        LIMIT 0, 1000;
-           
-";
-    
-        }elseif($carrera === 'ing'){
-            $query .=  "
-            CONCAT(t1.anio, '-', t1.semestre) AS periodos,
-            pr.nombre AS carrera,
-            CASE
-                WHEN t2.acum_matriculados > 0 THEN
-                    ROUND((t2.acum_retirados / t2.acum_matriculados) * 100, 2)
-                ELSE
-                    0
-            END AS tasa_desercion
-        FROM
-            periodo t1
-        LEFT JOIN (
-            SELECT
-                p.id_periodo,
-                SUM(DISTINCT pr.id_matricula) AS acum_matriculados,
-                SUM(DISTINCT r.id_retiro) AS acum_retirados
-            FROM
-                periodo p
-            LEFT JOIN matriculado pr ON p.id_periodo = pr.id_periodo
-            LEFT JOIN retirado r ON p.id_periodo = r.id_periodo
-            GROUP BY
-                p.id_periodo
-        ) t2 ON t1.id_periodo = t2.id_periodo
-        LEFT JOIN total t ON t1.id_periodo = t.id_periodo
-        LEFT JOIN programa pr ON t.id_programa = pr.id_programa
-        WHERE
-            pr.nombre = 'INGENIERIA EN TELEMATICA (CICLOS PROPEDEUTICOS)' 
-        ORDER BY
-            t1.anio, t1.semestre
-        LIMIT 0, 1000;
-        ";
-    
-        }
-       
-       
-        $result = $conn->query($query);
-        
-       
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                if($carrera=== 'all'){
-                    $periodo[] = $row['periodos'];
-                    $desercion[] = $row['tasa_desercion'];
-                                        
-                }else if($carrera === 'tec'){
-                    $periodoTec[] = $row['periodos'];
-                    $desercionTec[] = $row['tasa_desercion'];
-              }else if($carrera === 'ing'){
-                $periodoIng[] = $row['periodos'];
-                $desercionIng[] = $row['tasa_desercion'];
+                  ) AS UNSIGNED
+                ) AS tasa_desercion
+              FROM total t
+              JOIN periodo ON t.id_periodo = periodo.id_periodo
+              LEFT JOIN (
+                SELECT id_programa, matriculados, id_periodo
+                FROM total
+              ) AS t_anterior ON t.id_programa = t_anterior.id_programa AND t.id_periodo = t_anterior.id_periodo + 1
+              WHERE t.id_programa = 678 -- Filtrar por id_programa igual a 578
+              GROUP BY periodo.anio, periodo.cohorte
+              ORDER BY periodos
+              LIMIT 0, 1000;";
             }
-               
-            }
+
          
-        }
+
+            $result = $conn->query($query);
+
+          
+            if ($result) {
+                $sumaPonderada = 0;
+                $totalPeriodos = 0;
+                while ($row = $result->fetch_assoc()) {
+                    $totalPeriodos++;
+                    $sumaPonderada += floatval($row['tasa_desercion']); 
+                    $promedioPonderado = ($totalPeriodos > 0) ? ($sumaPonderada / $totalPeriodos) : 0;
+                    if($carrera=== 'all'){
+                        $cohortes[] = $row['periodos'];
+                        $desercions[] = $promedioPonderado;
+        
+    } else if($carrera === 'tec'){
+    $cohortesTec[] = $row['periodos'];
+    $desercionsTec[] = $promedioPonderado;
+    } else if($carrera === 'ing'){
+    $cohortesIng[] = $row['periodos'];
+    $desercionsIng[] = $promedioPonderado;
     }
-    
-       // Obtener datos por defecto (Todas las carreras)
-        fetchData('all');
-        fetchData('tec');
-        fetchData('ing');
-  
+    }
+    } else {
+    echo "<script>
+    console.error('Error en la consulta: " . $conn->error . "');
+    </script>";
+    }
+
+    }
+
+    // Obtener datos por defecto (Todas las carreras)
+    fetchData('all');
+    fetchData('tec');
+    fetchData('ing');
     ?>
 
     // Crear la gráfica inicial
-    var ctx = document.getElementById('retencion-chart').getContext('2d');
+    var ctx = document.getElementById('desercion-chart').getContext('2d');
     var chartTypeSelect = document.getElementById('chart-type');
     var chartCarreraSelect = document.getElementById('chart-carrera');
     var chart;
 
-    // Función para crear el gráfico de retención
+    // Función para crear el gráfico
     function createChart(chartType, labels, dataset) {
-        if (chart) {
-            chart.destroy(); // Destruir el gráfico existente si ya se ha creado
-        }
-
-        var data = {
-            labels: labels,
-            datasets: [dataset]
-        };
-
-        var options = {
-            responsive: true,
-            maintainAspectRatio: false
-        };
-
-        chart = new Chart(ctx, {
-            type: chartType,
-            data: data,
-            options: options
-        });
-
+    if (chart) {
+    chart.destroy(); // Destruir el gráfico existente si ya se ha creado
     }
 
+    var data = {
+    labels: labels,
+    datasets: [dataset]
+    };
+
+    var options = {
+    responsive: true,
+    maintainAspectRatio: false
+    };
+
+    chart = new Chart(ctx, {
+    type: chartType,
+    data: data,
+    options: options
+    });
+    }
 
     // Función para actualizar el gráfico con la carrera seleccionada
     // Función para actualizar el gráfico con la carrera seleccionada
     function updateChart() {
-        var selectedType = chartTypeSelect.value;
-        var selectedCarrera = chartCarreraSelect.value;
-        var dataset = [];
-        var labels; // Agregar esta línea para definir 'labels'
+    var selectedType = chartTypeSelect.value;
+    var selectedCarrera = chartCarreraSelect.value;
+    var dataset;
 
-        // Obtener el elemento del eje Y
-        var yAxis = chart.options.scales.y;
-        if (selectedCarrera === 'all') {
-            labels = <?php echo json_encode($periodo); ?>;
-        } else if (selectedCarrera === 'tec') {
-            labels = <?php echo json_encode($periodoTec); ?>;
-        } else if (selectedCarrera === 'ing') {
-            labels = <?php echo json_encode($periodoIng); ?>;
-        }
-
-        if (selectedCarrera === 'all') {
-            dataset = {
-                label: 'Deserción todas las carreras',
-                data: <?php echo json_encode($desercion); ?>,
-                backgroundColor: 'rgba(255, 159, 49, 0.87)',
-                borderColor: 'rgba(255, 159, 49, 0.87)',
-                borderWidth: 1
-            };
-        } else if (selectedCarrera === 'tec') {
-            dataset = {
-                label: 'Deserción TECNOLOGIA EN SISTEMATIZACION DE DATOS',
-                data: <?php echo json_encode($desercionTec); ?>,
-                backgroundColor: 'rgba(54, 162, 235, 0.5)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
-            };
-        } else if (selectedCarrera === 'ing') {
-            dataset = {
-                label: 'Deserción INGENIERIA EN TELEMATICA (CICLOS PROPEDEUTICOS)',
-                data: <?php echo json_encode($desercionIng); ?>,
-                backgroundColor: 'rgba(0, 255, 0, 0.58)',
-                borderColor: 'rgba(0, 255, 0, 0.58)',
-                borderWidth: 1
-            };
-        }
-
-
-        createChart(selectedType, labels, dataset);
-
-
+    if (selectedCarrera === 'all') {
+    dataset = {
+    label: 'TODAS LAS CARRERAS',
+    data: <?php echo json_encode($desercions); ?>,
+    backgroundColor: 'rgba(255, 159, 49, 0.87)',
+    borderColor: 'rgba(255, 159, 49, 0.87)',
+    borderWidth: 1
+    };
+    } else if (selectedCarrera === 'tec') {
+    dataset = {
+    label: 'TECNOLOGIA EN SISTEMATIZACION DE DATOS',
+    data: <?php echo json_encode($desercionsTec); ?>,
+    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+    borderColor: 'rgba(54, 162, 235, 1)',
+    borderWidth: 1
+    };
+    } else if (selectedCarrera === 'ing') {
+    dataset = {
+    label: 'INGENIERIA EN TELEMATICA',
+    data: <?php echo json_encode($desercionsIng); ?>,
+    backgroundColor: 'rgba(0, 255, 0, 0.58)',
+    borderColor: 'rgba(0, 255, 0, 0.58)',
+    borderWidth: 1
+    };
     }
 
+    createChart(selectedType, <?php echo json_encode($cohortes); ?>, dataset);
+    }
 
 
     // Evento de cambio de tipo de gráfico
@@ -271,12 +245,12 @@ $conn = $dbConnection->getDBConnection();
     chartCarreraSelect.addEventListener('change', updateChart);
 
     // Crear el gráfico inicial
-    createChart(chartTypeSelect.value, <?php echo json_encode($periodo); ?>, {
-        label: 'Todas las carreras',
-        data: <?php echo json_encode($desercion); ?>,
-        backgroundColor: 'rgba(255, 159, 49, 0.87)',
-        borderColor: 'rgba(255, 159, 49, 0.87)',
-        borderWidth: 1
+    createChart(chartTypeSelect.value, <?php echo json_encode($cohortes); ?>, {
+    label: 'Todas las carreras',
+    data: <?php echo json_encode($desercions); ?>,
+    backgroundColor: 'rgba(255, 159, 49, 0.87)',
+    borderColor: 'rgba(255, 159, 49, 0.87)',
+    borderWidth: 1
     });
     </script>
     <script>
